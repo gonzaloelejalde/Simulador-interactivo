ea266e225706f68d37bafd9d74b5d0560a2c5536
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-07-05 18:11:36 -0300

    agregue metodos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 do {
     valorPrestamo = parseInt (prompt("Ingrese un prestamo"))
 
@@ -8,27 +9,35 @@ do {
     if(valorPrestamo === 0){
         alert("No se puede pedir un préstamo por 0")
     }
+    if(true){
+
+    }
 
 }while((isNaN(valorPrestamo)) || (valorPrestamo === 0))
 
-function prestamos () {
-    class Prestamo {
-        constructor (impuestos, cuotas, IVA, precioFinal, aumentarPrecio) {
-            this.impuestos = impuestos
-            this.cuotas = cuotas
-            this.IVA = IVA
-            this.precioFinal = precioFinal
-            this.aumentarPrecio = aumentarPrecio
-        }
+
+class Prestamo {
+    constructor (valorPrestamo, IVA) {
+        this.valorPrestamo = valorPrestamo;
+        this.IVA = IVA;
+        this.impuestos = impuestos
+        this.precioFinal = this.valorPrestamo * IVA;
+    }
+    aplicarCuotas(){
+        this.cuotas = this.precioFinal / 3
+        this.precioAumento = (this.precioFinal * 1.05) / 6
+    }
+    mostrarMensaje(){
+        alert(`Cuando devuelvas el préstamo, vas a tener que pagar ${this.impuestos(valorPrestamo, IVA)} pesos argentinos, ya que aumenta por la inflación. Lo podes abonar en 3 cuotas de ${this.cuotas} sin interés o en 6 cuotas de ${this.precioAumento} con un interés del 5%, ambas con el aumento de los impuestos sobre el valor añadido`)
     }
 }
 
-this.impuestos = (num1, num2) => num1 * num2
 this.IVA = 1.21 
+
+this.impuestos = (num1, num2) => num1 * num2
 this.precioFinal = this.impuestos(valorPrestamo, IVA)
-this.cuotas = this.precioFinal / 3
-this.aumentarPrecio = (this.precioFinal * 1.05) / 6
 
-alert(`Cuando devuelvas el préstamo, vas a tener que pagar ${this.impuestos(valorPrestamo, IVA)} pesos argentinos, ya que aumenta por la inflación. Lo podes abonar en 3 cuotas de ${this.cuotas} sin interés o en 6 cuotas de ${this.aumentarPrecio} con un interés del 5%, ambas con el aumento de los impuestos sobre el valor añadido`)
+const prestamo1 = new Prestamo(valorPrestamo,IVA)
 
-alert("Si quiere probar con otro valor reinicie la página")
+prestamo1.aplicarCuotas()
+prestamo1.mostrarMensaje()
